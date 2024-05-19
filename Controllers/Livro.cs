3c984f002e07c414f878ce2a0e5ab266bbad47fd
leonardo <lupadinho867@gmail.com>
2024-05-19 11:49:43 -0300

    subindo projeto

---
+++ Controllers/Livro.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Livraria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Livro : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("create")]
+        [ProducesResponseType(typeof(Livros), StatusCodes.Status201Created)]
+        public IActionResult Create([FromBody] Livros request)
+        {
+            Livros.livrosLista.Add(request);
+            return Created(string.Empty, request);
+        }
+
+        [HttpGet]
+        [Route("buscar")]
+        [ProducesResponseType(typeof(Livros), StatusCodes.Status200OK)]
+        public IActionResult GetAll()
+        {  
+            return Ok(Livros.livrosLista);
+        }
+
+        [HttpPut]
+        [Route("editar")]
+        [ProducesResponseType(typeof(Livros), StatusCodes.Status204NoContent)]
+        public IActionResult Editar(int IdEdit, Livros request)
+        {
+            var item = Livros.livrosLista.FirstOrDefault(x => x.Id == IdEdit); 
+            if (item != null)
+            {
+                item.Autor = request.Autor;
+                item.QuantidadeEstoque = request.QuantidadeEstoque;
+                item.Titulo = request.Titulo;
+                item.Genero = request.Genero;
+                item.Preco = request.Preco;
+                return Created();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("deletar")]
+        [ProducesResponseType(typeof(Livros), StatusCodes.Status204NoContent)]
+        public IActionResult Deletar(int IdDelete)
+        {
+            var item = Livros.livrosLista.FirstOrDefault(x => x.Id == IdDelete);
+            if (item != null)
+            {
+                Livros.livrosLista.Remove(item);
+                return Created();
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Livros.cs
@@ -0,0 +1,15 @@
+﻿namespace Livraria
+{
+    public class Livros
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; } = string.Empty;
+        public string Genero { get; set; } = string.Empty;
+        public float Preco { get; set; }
+        public int QuantidadeEstoque { get; set; }
+
+        public static List<Livros> livrosLista = new List<Livros>();
+
+    }
+}
